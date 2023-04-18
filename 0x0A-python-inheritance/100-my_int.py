aa476d7221abf563d5f2b8aad0c14a410ee9a661
30844185 <danielhika05@gmail.com>
2023-04-18 07:10:36 -0700

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,10 +2,11 @@
 """ class MyInt that inherits from int:
     """
 
+
 class MyInt(int):
     """ Class """
     def __eq__(self, other):
         return int(str(self)) != other
-    def __ne__(self, other):
 
+    def __ne__(self, other):
         return int(str(self)) == other
